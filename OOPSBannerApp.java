e3ece50c90edad89d328b0f88a32841a74ad33c0
Vidyut Singh <officialvidyut007@gmail.com>
2026-03-11 09:48:21 +0530

    fix: update author name in OOPSBannerApp documentation

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -5,7 +5,7 @@
  * ASCII art composed of '*' and spaces in a 7-line format.
  *
  * @author Vidyut Singh Baruah
- * @version 2.0
+ * @version 3.0
  */
 public class OOPSBannerApp {
     public static void main(String[] args) {
