51c725621bc5cbdd87ea8dc01c7d4bcfe3a77fd8
Vidyut Singh <officialvidyut007@gmail.com>
2026-03-11 09:40:00 +0530

    feat(UC2): OOPS ASCII banner output in 7-line format and version 2.0

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,20 @@
 /**
- * OOPSBannerApp UC1 - OOPS Banner Display Application
+ * OOPSBannerApp UC2 - OOPS Large Banner Display Application
  *
- * This class demonstrates a simple Java application that displays the Object
- * Oriented Programming System OOPS acronym to the console.
+ * This class demonstrates a simple Java application that outputs OOPS as
+ * ASCII art composed of '*' and spaces in a 7-line format.
  *
- * @author Vidyut Singh Baruah
- * @version 1.0
+ * @author Developer
+ * @version 2.0
  */
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        System.out.println(" ***  " + "***  " + "***** " + " ***  ");
+        System.out.println("*   * " + "*   * " + "*    *" + "*   * ");
+        System.out.println("*   * " + "*   * " + "*    *" + "*  *  ");
+        System.out.println("*   * " + "*   * " + "***** " + " ***  ");
+        System.out.println("*   * " + "*   * " + "*     " + "    * ");
+        System.out.println("*   * " + "*   * " + "*     " + "*   * ");
+        System.out.println(" ***  " + " ***  " + "*     " + " ***  ");
     }
 }
