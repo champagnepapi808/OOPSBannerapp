0b8dff5ce7fc68875b5f9f792936a5c0c5e3bf6c
Vidyut Singh <officialvidyut007@gmail.com>
2026-03-11 09:55:15 +0530

    UC6

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -5,7 +5,7 @@
  * ASCII art composed of '*' and spaces in a 7-line format.
  *
  * @author Vidyut Singh Baruah
- * @version 4.0
+ * @version 5.0
  */
 public class OOPSBannerApp {
     public static void main(String[] args) {
